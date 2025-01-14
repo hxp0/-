0816b400c64400508fac1df20e4f410b33674794
周凯玉 <yu20011217@126.com>
2025-01-14 16:06:37 +0800

    登录页

--- react-ts-app/src/pages/login/Login.tsx
+++ react-ts-app/src/pages/login/Login.tsx
@@ -1,194 +1,139 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './Login.module.scss'
 import {
-  AlipayCircleOutlined,
   LockOutlined,
-  MobileOutlined,
-  TaobaoCircleOutlined,
   UserOutlined,
-  WeiboCircleOutlined,
 } from '@ant-design/icons';
 import {
   LoginForm,
   ProConfigProvider,
-  ProFormCaptcha,
-  ProFormCheckbox,
   ProFormText,
-  setAlpha,
-} from '@ant-design/pro-components';
-import { Space, Tabs, message, theme } from 'antd';
-import type { CSSProperties } from 'react';
-import { useState } from 'react';
+} from '@ant-design/pro-components'
+import { message, theme } from 'antd'
+import { getCaptchaApi, getLoginApi } from '../../services'
+import type { LoginParams } from '../../services/type'
+import { useNavigate } from 'react-router-dom'
 
-
-type LoginType = 'phone' | 'account';
-
-const items = [
-  { label: '我是学生', key: 'account',  }, 
-  { label: '我是老师', key: 'mobile', },
-];
 
 const Login: React.FC = () => {
-  const { token } = theme.useToken();
-  const [loginType, setLoginType] = useState<LoginType>('phone');
+  const navigate = useNavigate()
+  const { token } = theme.useToken()
+  const [imgUrl, setImgUrl] = useState<string | null>()
 
-  const iconStyles: CSSProperties = {
-    marginInlineStart: '16px',
-    color: setAlpha(token.colorTextBase, 0.2),
-    fontSize: '24px',
-    verticalAlign: 'middle',
-    cursor: 'pointer',
-  };
+  const getCaptcha = async()=>{
+    const res = await getCaptchaApi();
+    // console.log(res.data.data.code)
+    setImgUrl(res.data.data.code)
+  }
+
+  useEffect(()=>{
+    getCaptcha()
+  }, [])
+
+  const onFinish = async(values: LoginParams)=>{
+    try{
+      const res = await getLoginApi(values)
+      if( res.data.code === 1005 ){
+        message.error(res.data.msg)
+        getCaptcha()
+      }else if( res.data.code === 200 ){
+        localStorage.setItem('token', res.data.data?.code!)
+        navigate('/')
+        message.success('登录成功')
+      }
+    }catch(e){
+      console.log(e)
+    }
+  }
 
   return (
     <div className={style.login}>  
-      <ProConfigProvider hashed={false}>
+      <ProConfigProvider
+        hashed={false}
+      >
         <div style={{ backgroundColor: token.colorBgContainer }}>
           <LoginForm
-            logo="https://github.githubassets.com/favicons/favicon.png"
             title="OnlineExam"
             subTitle="在线考试平台"
-            actions={
-              <Space>
-                其他登录方式
-                <AlipayCircleOutlined style={iconStyles} />
-                <TaobaoCircleOutlined style={iconStyles} />
-                <WeiboCircleOutlined style={iconStyles} />
-              </Space>
-            }
+            onFinish={onFinish}
           >
-            <Tabs
-              centered
-              activeKey={loginType}
-              items={items}
-              onChange={(activeKey) => setLoginType(activeKey as LoginType)}
-            ></Tabs>
-            {loginType === 'account' && (
-              <>
-                <ProFormText
-                  name="username"
-                  fieldProps={{
-                    size: 'large',
-                    prefix: <UserOutlined className={'prefixIcon'} />,
-                  }}
-                  placeholder={'用户名: admin or user'}
-                  rules={[
-                    {
-                      required: true,
-                      message: '请输入用户名!',
-                    },
-                  ]}
-                />
-                <ProFormText.Password
-                  name="password"
-                  fieldProps={{
-                    size: 'large',
-                    prefix: <LockOutlined className={'prefixIcon'} />,
-                    strengthText:
-                      'Password should contain numbers, letters and special characters, at least 8 characters long.',
-                    statusRender: (value) => {
-                      const getStatus = () => {
-                        if (value && value.length > 12) {
-                          return 'ok';
-                        }
-                        if (value && value.length > 6) {
-                          return 'pass';
-                        }
-                        return 'poor';
-                      };
-                      const status = getStatus();
-                      if (status === 'pass') {
-                        return (
-                          <div style={{ color: token.colorWarning }}>
-                            强度：中
-                          </div>
-                        );
-                      }
-                      if (status === 'ok') {
-                        return (
-                          <div style={{ color: token.colorSuccess }}>
-                            强度：强
-                          </div>
-                        );
-                      }
-                      return (
-                        <div style={{ color: token.colorError }}>强度：弱</div>
-                      );
-                    },
-                  }}
-                  placeholder={'密码: ant.design'}
-                  rules={[
-                    {
-                      required: true,
-                      message: '请输入密码！',
-                    },
-                  ]}
-                />
-              </>
-            )}
-            {loginType === 'phone' && (
-              <>
-                <ProFormText
-                  fieldProps={{
-                    size: 'large',
-                    prefix: <MobileOutlined className={'prefixIcon'} />,
-                  }}
-                  name="mobile"
-                  placeholder={'手机号'}
-                  rules={[
-                    {
-                      required: true,
-                      message: '请输入手机号！',
-                    },
-                    {
-                      pattern: /^1\d{10}$/,
-                      message: '手机号格式错误！',
-                    },
-                  ]}
-                />
-                <ProFormCaptcha
-                  fieldProps={{
-                    size: 'large',
-                    prefix: <LockOutlined className={'prefixIcon'} />,
-                  }}
-                  captchaProps={{
-                    size: 'large',
-                  }}
-                  placeholder={'请输入验证码'}
-                  captchaTextRender={(timing, count) => {
-                    if (timing) {
-                      return `${count} ${'获取验证码'}`;
-                    }
-                    return '获取验证码';
-                  }}
-                  name="captcha"
-                  rules={[
-                    {
-                      required: true,
-                      message: '请输入验证码！',
-                    },
-                  ]}
-                  onGetCaptcha={async () => {
-                    message.success('获取验证码成功！验证码为：1234');
-                  }}
-                />
-              </>
-            )}
-            <div
-              style={{
-                marginBlockEnd: 24,
+            <ProFormText
+              name="username"
+              fieldProps={{
+                size: 'large',
+                prefix: <UserOutlined className={'prefixIcon'} />,
               }}
-            >
-              <ProFormCheckbox noStyle name="autoLogin">
-                自动登录
-              </ProFormCheckbox>
-              <a
-                style={{
-                  float: 'right',
+              placeholder={'用户名'}
+              rules={[
+                {
+                  required: true,
+                  message: '请输入用户名!',
+                },
+              ]}
+            />
+            <ProFormText.Password
+              name="password"
+              fieldProps={{
+                size: 'large',
+                prefix: <LockOutlined className={'prefixIcon'} />,
+                strengthText:
+                  'Password should contain numbers, letters and special characters, at least 8 characters long.',
+                statusRender: (value) => {
+                  const getStatus = () => {
+                    if (value && value.length > 12) {
+                      return 'ok';
+                    }
+                    if (value && value.length > 6) {
+                      return 'pass';
+                    }
+                    return 'poor';
+                  };
+                  const status = getStatus();
+                  if (status === 'pass') {
+                    return (
+                      <div style={{ color: token.colorWarning }}>
+                        强度：中
+                      </div>
+                    );
+                  }
+                  if (status === 'ok') {
+                    return (
+                      <div style={{ color: token.colorSuccess }}>
+                        强度：强
+                      </div>
+                    );
+                  }
+                  return (
+                    <div style={{ color: token.colorError }}>强度：弱</div>
+                  );
+                },
+              }}
+              placeholder={'密码'}
+              rules={[
+                {
+                  required: true,
+                  message: '请输入密码！',
+                },
+              ]}
+            />
+            <div className={style.code}> 
+              <ProFormText
+                name="code"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined className={'prefixIcon'} />,
                 }}
-              >
-                忘记密码
-              </a>
+                placeholder={'验证码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入验证码!',
+                  },
+                ]}
+              />
+              <div className={style.codeImg} onClick={getCaptcha}>
+                <img src={imgUrl!} alt="" />
+              </div>
             </div>
           </LoginForm>
         </div>
--- react-ts-app/src/services/index.ts
+++ react-ts-app/src/services/index.ts
@@ -1,7 +1,16 @@
 import axios from 'axios'
+import { CaptchaType, LoginParams, LoginType } from './type'
 
-export const getInfoApi = async()=>{
-  const res = await axios.get('https://zyxcl.xyz/exam_api/user/info')
-  // const res
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? '/api' : 'https://zyxcl.xyz/exam_api'
+
+// 获取验证码
+export const getCaptchaApi = async()=>{
+  const res = await axios.get<CaptchaType>('/login/captcha')
+  return res
+}
+
+// 登录接口
+export const getLoginApi = async( params:LoginParams )=>{
+  const res = await axios.post<LoginType>('/login', params)
   return res
 }
